33adffa4b9d78c5a45df69b2619c2dd483e66504
Kev Kirkland <kev@dataunity.org>
2018-02-28 17:59:46 +0000

    Add limit line to 24 hour mean chart.

--- website/js/site.js
+++ website/js/site.js
@@ -1,110 +1,22 @@
-var valField = "P1",
-    dateField = "timestamp";
+"use strict";
 
-var spec = {
-    "$schema": "https://vega.github.io/schema/vega/v3.json",
-    "width": 500,
-    "height": 200,
-    "padding": 5,
+var luftviz = luftviz || {};
 
-    "data": [
-        {
-            "name": "table",
-            "url": "/data/luftdaten_sds011_sensor_7675_24_hour_means.csv",
-            "format": {
-                "type": "csv",
-                "parse": "auto"
-            }
-        }
-    ],
-
-    "scales": [
-        {
-            "name": "x",
-            "type": "time",
-            "range": "width",
-            "domain": {"data": "table", "field": dateField}
-        },
-        {
-            "name": "y",
-            "type": "linear",
-            "range": "height",
-            "nice": true,
-            "zero": true,
-            "domain": {"data": "table", "field": valField}
-        },
-        {
-            "name": "color",
-            "type": "ordinal",
-            "range": "category",
-            "domain": {"data": "table", "field": valField}
-        }
-    ],
-
-    "axes": [
-        {
-            "orient": "bottom",
-            "scale": "x",
-            "format": "%-m %b %y",
-            "labelOverlap": "true"
-        },
-        {
-            "orient": "left",
-            "scale": "y"
-        }
-    ],
-
-    "marks": [
-        // {
-        //     "type": "line",
-        //     "from": {"data": "table"},
-        //     "encode": {
-        //         "enter": {
-        //             "x": {"scale": "x", "field": dateField},
-        //             "y": {"scale": "y", "field": valField},
-        //             // "stroke": {"scale": "color", "field": "c"},
-        //             "strokeWidth": {"value": 2}
-        //         },
-        //         "update": {
-        //             "fillOpacity": {"value": 1}
-        //         },
-        //         "hover": {
-        //             "fillOpacity": {"value": 0.5}
-        //         }
-        //     }
-        // },
-        {
-            "type": "symbol",
-            "from": {"data": "table"},
-            "encode": {
-                "enter": {
-                    "x": {"scale": "x", "field": dateField},
-                    "y": {"scale": "y", "field": valField},
-                    "fill": {"value": "#ff0000"},
-                    // "stroke": {"value": "#000"},
-                    // "strokeWidth": {"value": 1},
-                    "size": {"value": 5}
-                }
-            }
-        }
-    ]
-};
-
-var opt = {};
-
-vegaEmbed("#view2", spec, opt).then(function (result) {
-    var tooltipOption = {
+luftviz.chart24hourmean = (function (d3, vega) {
+    // Private properties
+    var valField = "P1",
+        dateField = "timestamp",
+        dateFormat = "%-d %b %y",
+        euLimitPM10 = 50,
+        euLimitPM2point5 = 50,
+        vegaTooltipOptions = {
             showAllFields: false,
             fields: [
                 {
                     field: dateField,
                     title: "Date",
                     formatType: "time",
-                    format: "%B %e",
-                    valueAccessor: function (d) {
-                        console.log(d);
-                        return 1;
-                    }
+                    format: dateFormat + " %H:%M"
                 },
                 {
                     field: valField,
@@ -112,10 +24,193 @@ vegaEmbed("#view2", spec, opt).then(function (result) {
                 }
 
             ]
+        },
+
+        // Private methods
+        createSpec = function (sensorId, data, valField, dateField) {
+            // Creates a vega spec
+            var minMaxDates, minDate, maxDate, limitValues;
+
+            // Find the min and max dates
+            minMaxDates = d3.extent(data.map(d => d[dateField]));
+            minDate = minMaxDates[0];
+            maxDate = minMaxDates[1];
+
+            // Create data for EU remmended limits line
+            limitValues = [
+                {
+                    "date": minDate,
+                    "value": euLimitPM10
+                },
+                {
+                    "date": maxDate,
+                    "value": euLimitPM10
+                }
+            ];
+
+            var spec = {
+                "$schema": "https://vega.github.io/schema/vega/v3.json",
+                "width": 500,
+                "height": 200,
+                "padding": 5,
+
+                "data": [
+                    {
+                        "name": "table",
+                        "values": data
+                        // "url": "/data/luftdaten_sds011_sensor_" + sensorId + "_24_hour_means.csv",
+                        // "format": {
+                        //     "type": "csv",
+                        //     "parse": "auto"
+                        // }
+                    },
+                    {
+                        "name": "limitEU",
+                        "values": limitValues
+                    }
+                ],
+
+                "scales": [
+                    {
+                        "name": "x",
+                        "type": "time",
+                        "range": "width",
+                        "domain": {"data": "table", "field": dateField}
+                        // "nice": true
+                        // "interval": "week", "step": 1
+                    },
+                    {
+                        "name": "y",
+                        "type": "linear",
+                        "range": "height",
+                        "nice": true,
+                        "zero": true,
+                        "domain": {"data": "table", "field": valField}
+                    },
+                    {
+                        "name": "color",
+                        "type": "ordinal",
+                        "range": "category",
+                        "domain": {"data": "table", "field": valField}
+                    },
+                    {
+                        "name": "colorPM",
+                        "type": "ordinal",
+                        "range": "category",
+                        "domain": {"data": "table", "field": valField}
+                    }
+                ],
+
+                "axes": [
+                    {
+                        "orient": "bottom",
+                        "scale": "x",
+                        "format": dateFormat,
+                        // "format": "%-m %b %y",
+                        "labelOverlap": "true"
+                    },
+                    {
+                        "orient": "left",
+                        "scale": "y"
+                    }
+                ],
+
+                "marks": [
+                    // {
+                    //     "type": "line",
+                    //     "from": {"data": "table"},
+                    //     "encode": {
+                    //         "enter": {
+                    //             "x": {"scale": "x", "field": dateField},
+                    //             "y": {"scale": "y", "field": valField},
+                    //             // "stroke": {"scale": "color", "field": "c"},
+                    //             "strokeWidth": {"value": 2}
+                    //         },
+                    //         "update": {
+                    //             "fillOpacity": {"value": 1}
+                    //         },
+                    //         "hover": {
+                    //             "fillOpacity": {"value": 0.5}
+                    //         }
+                    //     }
+                    // },
+                    // Sensor data
+                    {
+                        "type": "symbol",
+                        "from": {"data": "table"},
+                        "encode": {
+                            "enter": {
+                                "x": {"scale": "x", "field": dateField},
+                                "y": {"scale": "y", "field": valField},
+                                "fill": {"value": "#ff0000"},
+                                // "stroke": {"value": "#000"},
+                                // "strokeWidth": {"value": 1},
+                                "size": {"value": 5}
+                            }
+                        }
+                    },
+                    // Limits
+                    {
+                        "type": "line",
+                        "from": {"data": "limitEU"},
+                        "encode": {
+                            "enter": {
+                                "x": {"scale": "x", "field": "date"},
+                                "y": {"scale": "y", "field": "value"},
+                                // "stroke": {"scale": "color", "field": "c"},
+                                "strokeWidth": {"value": 2}
+                            },
+                            "update": {
+                                "fillOpacity": {"value": 1}
+                            },
+                            "hover": {
+                                "fillOpacity": {"value": 0.5}
+                            }
+                        }
+                    }
+                ]
+            };
+            return spec;
+        },
+        render = function (el, sensorId) {
+            var dataUrl = "/data/luftdaten_sds011_sensor_" + sensorId + "_24_hour_means.csv";
+            d3.csv(dataUrl, function(data) {
+                // Set data types
+                var parseDate = d3.timeParse("%Y-%m-%d %H:%M:%S");
+                data.forEach(function(d) {
+                    d[dateField] = parseDate(d[dateField]);
+                    d.P1 = +d.P1;
+                });
+
+                var specCopy = createSpec(sensorId, data, valField, dateField),
+                    view = new vega.View(vega.parse(specCopy))
+                        .renderer('canvas')  // set renderer (canvas or svg)
+                        .initialize(el) // initialize view within parent DOM container
+                        .hover()             // enable hover encode set processing
+                        .run();
+                vegaTooltip.vega(view, vegaTooltipOptions);
+            });
         };
-        vegaTooltip.vega(result.view, tooltipOption);
-        // vegaTooltip.vega(result.view);
-    }).catch(console.error);
+
+    // Public interface
+    return {
+        render: render
+    }
+} (d3, vega));
+
+
+
+
+// var opt = {};
+// var view = null;
+
+
+// vegaEmbed("#view2", spec).then(function (result) {
+//         vegaTooltip.vega(result.view, tooltipOption);
+//         // view = result.view;
+//         // vegaTooltip.vega(result.view);
+//     }).catch(console.error);
+
 
 // var view;
 //
@@ -125,5 +220,21 @@ vegaEmbed("#view2", spec, opt).then(function (result) {
 //         .initialize('#view') // initialize view within parent DOM container
 //         .hover()             // enable hover encode set processing
 //         .run();
+//     vegaTooltip.vega(view, vegaTooltipOptions);
 // }
 // render(spec);
+
+var scale = d3.scaleBand()
+    .domain([1, 2, 3])
+    .range([0, 100]);
+console.log(scale(1))
+console.log(scale(2))
+console.log(scale(3))
+
+var scaleOrd = d3.scaleOrdinal()
+    .domain([1, 2, 3])
+    .range([d3.rgb("red"), d3.rgb("green"), d3.rgb("blue")]);
+console.log(scaleOrd(1))
+console.log(scaleOrd(1.5))
+console.log(scaleOrd(2))
+console.log(scaleOrd(3))
